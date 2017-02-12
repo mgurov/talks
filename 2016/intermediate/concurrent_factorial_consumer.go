0074299b87dc79e697d6b7f58f1fda70f5cbf180
Mykola Gurov <ngurov@gmail.com>
2017-02-12 21:50:19 +0100

    concurrent factorial: producer/consumer with waitgroup

---
+++ 2016/intermediate/concurrent_factorial_consumer.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// START OMIT
+
+func main() {
+	result := make(chan int)
+	wg := sync.WaitGroup{}
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		go produce(i, result, &wg)
+	}
+	go consume(result)
+	wg.Wait()
+}
+
+func consume(result chan int) {
+	for r := range result {
+		fmt.Println(r)
+	}
+}
+
+func produce(i int, c chan int, wg *sync.WaitGroup) {
+	c <- fac(i)
+	wg.Done()
+}
+// END OMIT
+
+func fac(i int) int {
+	if i == 1 {
+		return 1
+	} else {
+		return i * fac(i-1)
+	}
+}
